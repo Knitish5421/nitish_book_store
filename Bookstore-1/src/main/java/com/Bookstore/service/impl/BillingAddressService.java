da28beb72ad6833d65fecbbb885e6f56e1911e6e
unknown <nitish5421@gmail.com>
2020-06-13 21:14:01 +0530

    change in billingservice

--- Bookstore-1/src/main/java/com/Bookstore/service/impl/BillingAddressService.java
+++ Bookstore-1/src/main/java/com/Bookstore/service/impl/BillingAddressService.java
@@ -5,6 +5,6 @@ import com.Bookstore.model.UserBilling;
 
 public interface BillingAddressService
 {
-	public BillingAddress setByUserBilling(UserBilling userBilling, BillingAddress billingAddress);
+	public BillingAddress setByUserBilling(UserBilling userBilling, BillingAddress billingAddre);
    
 }
